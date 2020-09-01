d594f73673c6975d9412b853d7a662025c6ada62
Pawel <p.buczynski@outlook.com>
2020-09-01 20:19:00 +0200

    Updates

---
+++ precache-manifest.4633c2bcfa21f5a065a324658acc2518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fefe93b23ef26fde308b0fa5505080",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "f3f7ca7171139ade8cef",
+    "url": "/weather/static/css/main.ed94b16a.chunk.css"
+  },
+  {
+    "revision": "d3d168e2432d2bca86e1",
+    "url": "/weather/static/js/2.a732cf67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.a732cf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f7ca7171139ade8cef",
+    "url": "/weather/static/js/main.5f560005.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
