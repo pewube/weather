9256730244d8c06901ef7b664b8e0582ce4a8552
Pawel <p.buczynski@outlook.com>
2020-09-02 13:18:20 +0200

    Updates

---
+++ precache-manifest.8176379a6bc19a93bafe7ab80c7b50a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202b22f002f375270473dd633b4a212e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec3b02d7c3213a7efc57",
+    "url": "./static/css/main.ed94b16a.chunk.css"
+  },
+  {
+    "revision": "d3d168e2432d2bca86e1",
+    "url": "./static/js/2.a732cf67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a732cf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3b02d7c3213a7efc57",
+    "url": "./static/js/main.79048968.chunk.js"
+  },
+  {
+    "revision": "7185123dbfbf19a73c74",
+    "url": "./static/js/runtime-main.3def4396.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.96c8fd454452c3907b275a8c9cc41aed.js"
+  "./precache-manifest.8176379a6bc19a93bafe7ab80c7b50a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
