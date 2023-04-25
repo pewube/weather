715984871e48ab8e36499b147fb8dce698123bfc
Pawel <p.buczynski@outlook.com>
2023-04-25 13:15:34 +0200

    FEAT: add homepage link and modal support

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 
 import { AppContext } from "../context/AppContext";
 import Footer from "../components/Footer";
 import Form from "../components/Form";
+import Modal from "../components/Modal";
 import Navigation from "../components/Navigation";
 
 const App = () => {
   const params = useParams();
-  const { appBackground } = useContext(AppContext);
+  const { appBackground, setAppBackground, setInputError } =
+    useContext(AppContext);
 
   const bgImage = (
     <img
@@ -22,7 +24,7 @@ const App = () => {
     Object.keys(params).length === 0 ? "header header--start" : "header";
 
   const navigation =
-    Object.keys(params).length === 0 || params.city ? null : (
+    Object.keys(params).length === 0 || !params.lat ? null : (
       <nav className="nav">
         <Navigation />
       </nav>
@@ -34,15 +36,23 @@ const App = () => {
       </main>
     );
 
+  const handleHomePageLinkClick = () => {
+    setAppBackground("home");
+    setInputError({ is: false, statusText: "" });
+  };
+
   return (
     <>
       {bgImage}
       <header className={headerClassName}>
-        <h1 className="header__title">Jaka pogoda ?</h1>
+        <Link to="/" onClick={handleHomePageLinkClick} aria-label="Home page">
+          <h1 className="header__title">Jaka pogoda ?</h1>
+        </Link>
         <Form />
       </header>
       {navigation}
       {main}
+      <Modal />
       <Footer />
     </>
   );
