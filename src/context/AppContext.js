deecc67990ad8277bd7bc896e9dc368902be91a0
Pawel <p.buczynski@outlook.com>
2023-04-25 13:14:03 +0200

    REFACTOR: update of modal support

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,6 +5,11 @@ export const AppContext = createContext();
 const AppProvider = (props) => {
   const [appBackground, setAppBackground] = useState("home");
   const [inputError, setInputError] = useState({ is: false, info: "" });
+  const [modal, setModal] = useState({
+    visible: false,
+    header: {},
+    body: {},
+  });
   const [weatherData, setWeatherData] = useState({});
 
   return (
@@ -12,9 +17,11 @@ const AppProvider = (props) => {
       value={{
         appBackground,
         inputError,
+        modal,
         weatherData,
         setAppBackground,
         setInputError,
+        setModal,
         setWeatherData,
       }}>
       {props.children}
