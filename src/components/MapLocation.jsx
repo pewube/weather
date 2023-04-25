594ee674d811126a7e609586bf392a6a1ec7762c
Pawel <p.buczynski@outlook.com>
2023-04-25 13:13:17 +0200

    FEAT: map component
    - use leaflet and react-leaflet,
    - features: marker, popup with link to forecast, minimap

---
+++ src/components/MapLocation.jsx
@@ -0,0 +1,111 @@
+import { useCallback, useMemo, useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  Rectangle,
+  TileLayer,
+  useMap,
+} from "react-leaflet";
+import { useEventHandlers } from "@react-leaflet/core";
+import { Link } from "react-router-dom";
+
+// Classes used by Leaflet to position controls
+const POSITION_CLASSES = {
+  bottomleft: "leaflet-bottom leaflet-left",
+  bottomright: "leaflet-bottom leaflet-right",
+  topleft: "leaflet-top leaflet-left",
+  topright: "leaflet-top leaflet-right",
+};
+
+const BOUNDS_STYLE = { weight: 1 };
+
+function MinimapBounds({ parentMap, zoom }) {
+  const minimap = useMap();
+
+  // Keep track of bounds in state to trigger renders
+  const [bounds, setBounds] = useState(parentMap.getBounds());
+  const onChange = useCallback(() => {
+    setBounds(parentMap.getBounds());
+    // Update the minimap's view to match the parent map's center and zoom
+    minimap.setView(parentMap.getCenter(), zoom);
+  }, [minimap, parentMap, zoom]);
+
+  // Listen to events on the parent map
+  const handlers = useMemo(
+    () => ({ move: onChange, zoom: onChange }),
+    [onChange]
+  );
+  useEventHandlers({ instance: parentMap }, handlers);
+
+  return <Rectangle bounds={bounds} pathOptions={BOUNDS_STYLE} />;
+}
+
+function MinimapControl({ position, zoom }) {
+  const parentMap = useMap();
+  const mapZoom = zoom || 0;
+
+  // Memoize the minimap so it's not affected by position changes
+  const minimap = useMemo(
+    () => (
+      <MapContainer
+        style={{ height: 80, width: 80 }}
+        center={parentMap.getCenter()}
+        zoom={mapZoom}
+        dragging={false}
+        doubleClickZoom={false}
+        scrollWheelZoom={false}
+        attributionControl={false}
+        zoomControl={false}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MinimapBounds parentMap={parentMap} zoom={mapZoom} />
+      </MapContainer>
+    ),
+    [mapZoom, parentMap]
+  );
+
+  const positionClass =
+    (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright;
+  return (
+    <div className={positionClass}>
+      <div className="leaflet-control leaflet-bar">{minimap}</div>
+    </div>
+  );
+}
+
+const MapLocation = ({
+  data: city,
+  zoom = 9,
+  minimap = false,
+  popup = false,
+}) => {
+  return (
+    <MapContainer
+      center={[city.lat, city.lon]}
+      zoom={zoom}
+      scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Marker position={[city.lat, city.lon]}>
+        {popup && (
+          <Popup>
+            <Link
+              to={`/${encodeURIComponent(city.name)}/${city.lat}/${
+                city.lon
+              }/current`}>
+              {city.local_names?.pl || city.name} - kliknij aby zobaczyć jaka
+              pogoda
+            </Link>
+          </Popup>
+        )}
+      </Marker>
+
+      {minimap && <MinimapControl position="topright" />}
+    </MapContainer>
+  );
+};
+
+export default MapLocation;
