5ca217785abe72e59ef13dfc3fd7a2de72debaa9
Pawel <p.buczynski@outlook.com>
2023-04-25 13:04:38 +0200

    REFACTOR: cities list component
    - externalize the city details formatting function
    - add support for map modal,
    - add support for flags,

--- src/components/CitiesList.jsx
+++ src/components/CitiesList.jsx
@@ -1,45 +1,59 @@
+import { useContext, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-const CitiesList = ({ cities }) => {
-  const formatCities = (citiesList) => {
-    for (const city of citiesList) {
-      if (city.state) {
-        city["state"] = city["state"]
-          .replace(" Voivodeship", "")
-          .replace("Masovian", "Mazowieckie")
-          .replace("Łódź", "Łódzkie");
-      }
-    }
-  };
+import { AppContext } from "../context/AppContext";
+import MapLocation from "./MapLocation";
 
-  formatCities(cities);
+import formatCityDetails from "../formatCityDetails";
+
+const CitiesList = ({ data }) => {
+  const { setAppBackground, setModal } = useContext(AppContext);
+
+  useEffect(() => {
+    setAppBackground("home");
+  }, [setAppBackground]);
+
+  const cities = formatCityDetails(data);
+
+  const handleMapBtn = (city) => {
+    const modalHeader = (
+      <h2 className="modal__header__title">
+        {city.local_names?.pl || city.name}
+      </h2>
+    );
+    const modalBody = <MapLocation data={city} minimap={true} popup={true} />;
+
+    setModal({ visible: true, header: modalHeader, body: modalBody });
+  };
 
   const citiesList = cities?.map((city, index) => (
     <li key={index} className="cities-list__item city">
-      <NavLink to={`/current/${city.lat}/${city.lon}`} className="city__link">
-        <span className="city__name">{city.local_names?.pl || city.name}</span>
-        <span className="city__country">{city.country || null}</span>
-        <span className="city__country-flag">flaga</span>
-        <span className="city__state">{city.state || null}</span>
-      </NavLink>
       <NavLink
-        to={`https://www.openstreetmap.org/?mlat=${city.lat}&mlon=${
-          city.lon
-        }#map=14/${city.lat.toFixed(4)}/${city.lon.toFixed(4)}`}
-        className="city__link city__coordinates"
-        target="_blank"
-        rel="noopener">
-        {city.lat && city.lon
-          ? city.lat.toFixed(4) + ", " + city.lon.toFixed(4)
-          : null}
+        to={`/${encodeURIComponent(city.name)}/${city.lat}/${city.lon}/current`}
+        className="city__link">
+        <div className="city__name">
+          <p>{city.local_names?.pl || city.name}</p>
+          {city.state && <p>{city.state || null}</p>}
+        </div>
+        <div className="city__country">
+          {city.country || null}
+          <span
+            className={`city__country__flag fi fi-${city.country.toLowerCase()}`}></span>
+        </div>
       </NavLink>
+      <div className="divider-vertical"></div>
+      <button
+        className="city__btn btn-clear"
+        onClick={() => handleMapBtn(city)}>
+        <span className="material-symbols-outlined">map</span>
+      </button>
     </li>
   ));
 
   return (
     <>
-      <header>
-        Znaleziono więcej niż jedno miasto. Wybierz właściwe z poniższej listy:
+      <header className="cities-list__header">
+        <p>Wybierz właściwą lokalizację z poniższej listy:</p>
       </header>
       <ul className="cities-list">{citiesList}</ul>
     </>
