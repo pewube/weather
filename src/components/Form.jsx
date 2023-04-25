53e40d0d299d25d85babaee917a1e3413bbb705b
Pawel <p.buczynski@outlook.com>
2023-04-25 13:07:48 +0200

    REFACTOR: form component
    - update of errors support,
    - add input content reset button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,13 +13,40 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (inputRef.current.value.length > 1) {
-      setInputError({ is: false, info: "" });
-      navigate(`/city/${inputRef.current.value}`);
+    if (inputRef.current.value.length > 2) {
+      setInputError({ is: false, statusText: "" });
+      navigate(`/city/${encodeURIComponent(inputRef.current.value)}`);
+      inputRef.current.value = "";
     } else {
-      setInputError({ is: true, info: "Nazwa miejscowości musi być dłuższa." });
+      setInputError({
+        is: true,
+        info: (
+          <>
+            <p>Nazwa miejscowości musi być dłuższa.</p>
+            <p>
+              Jeżeli próbujesz wyszukać miejscowość o nazwie krótszej niż 3
+              litery - po nazwie wpisz przecinek i 2-literowy kod kraju
+              (ISO3166) np.{" "}
+              <span className="example">
+                A, FR (dla miejscowości A we Francji)
+              </span>
+            </p>
+          </>
+        ),
+      });
     }
   };
+
+  const handleInputResetBtn = () => {
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
+  const inputErrorInfo = inputError.info && (
+    <section className="form__info info-box info-box--warning">
+      {inputError.info}
+    </section>
+  );
 
   return (
     <>
@@ -31,9 +58,15 @@ const Form = () => {
           aria-label="Search city"
           placeholder="Nazwa miejscowości"
         />
-        <button className="form__btn">Szukaj</button>
+        <button
+          className="form__input__btn btn-clear"
+          type="reset"
+          onClick={handleInputResetBtn}>
+          <span className="material-symbols-outlined">close</span>
+        </button>
+        <button className="form__btn btn-primary">Szukaj</button>
+        {inputErrorInfo}
       </form>
-      <p className="form__info">{inputError.info}</p>
     </>
   );
 };
