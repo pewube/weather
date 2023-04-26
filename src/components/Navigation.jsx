3313d9c1aea29f507430f5a111d2bca478fe0c6d
Pawel <p.buczynski@outlook.com>
2023-04-26 11:51:48 +0200

    REFACTOR: change paths, add classes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,25 +3,31 @@ import { NavLink, useParams } from "react-router-dom";
 const Navigation = () => {
   const params = useParams();
   return (
-    <ul>
+    <ul className="nav__list">
       <li className="nav__item">
         <NavLink
-          to={`/current/${params.lat}/${params.lon}`}
-          className="nav__item__link">
+          to={`/${encodeURIComponent(params.city)}/${params.lat}/${
+            params.lon
+          }/now`}
+          className="nav__item__link btn-primary">
           Teraz
         </NavLink>
       </li>
       <li className="nav__item">
         <NavLink
-          to={`/48-hours/${params.lat}/${params.lon}`}
-          className="nav__item__link">
-          48 godzin
+          to={`/${encodeURIComponent(params.city)}/${params.lat}/${
+            params.lon
+          }/48-hours`}
+          className="nav__item__link btn-primary">
+          48 godz.
         </NavLink>
       </li>
       <li className="nav__item">
         <NavLink
-          to={`/8-days/${params.lat}/${params.lon}`}
-          className="nav__item__link">
+          to={`/${encodeURIComponent(params.city)}/${params.lat}/${
+            params.lon
+          }/8-days`}
+          className="nav__item__link btn-primary">
           8 dni
         </NavLink>
       </li>
