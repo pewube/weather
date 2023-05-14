4c993367923dcc2cd0edff1eed4aa7727444dbc5
Pawel <p.buczynski@outlook.com>
2023-05-14 17:21:42 +0200

    CHORE: add file in new location (Forecast8Days)

---
+++ src/routes/Forecast8Days.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import DailyForecast from "../components/DailyForecast";
+
+const Forecast48Hours = () => {
+  const { weatherData } = useContext(AppContext);
+
+  const hourForecasts = weatherData?.forecast?.daily?.map((item) => (
+    <DailyForecast key={item.dt} data={item} />
+  ));
+
+  return (
+    <article className="days-8 info-box">
+      <header className="days-8__header">
+        <h2>Prognoza 48-dniowa</h2>
+      </header>
+      <ul className="days-8__list">{hourForecasts}</ul>
+    </article>
+  );
+};
+
+export default Forecast48Hours;
