d43f6a835cc290a1b750c33b5dcfda85ff4a1c38
Pawel <p.buczynski@outlook.com>
2023-05-14 17:45:44 +0200

    REFACTOR: adjust background images to display resolution, add spinner

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,28 @@
 import { useContext } from "react";
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigation, useParams } from "react-router-dom";
 
 import { AppContext } from "../context/AppContext";
 import Footer from "../components/Footer";
 import Form from "../components/Form";
 import Modal from "../components/Modal";
 import Navigation from "../components/Navigation";
+import Spinner from "../components/Spinner";
+
+import adjustBgRes from "../adjustBgRes";
 
 const App = () => {
   const params = useParams();
+  const navigation = useNavigation();
+
   const { appBackground, setAppBackground, setInputError } =
     useContext(AppContext);
+
+  const bgRes = adjustBgRes();
 
   const bgImage = (
     <img
       className="app-bg"
-      src={`/assets/img/bg-${appBackground}.jpg`}
+      src={`/assets/img/bg-${bgRes}-${appBackground}.webp`}
       alt=""
     />
   );
@@ -23,7 +30,7 @@ const App = () => {
   const headerClassName =
     Object.keys(params).length === 0 ? "header header--start" : "header";
 
-  const navigation =
+  const nav =
     Object.keys(params).length === 0 || !params.lat ? null : (
       <nav className="nav">
         <Navigation />
@@ -45,18 +52,22 @@ const App = () => {
     <>
       {bgImage}
       <header className={headerClassName}>
-        <Link to="/" onClick={handleHomePageLinkClick} aria-label="Home page">
+        <Link
+          to="/"
+          onClick={handleHomePageLinkClick}
+          className="header__home-link"
+          aria-label="Home page">
           <h1 className="header__title">Jaka pogoda ?</h1>
         </Link>
         <Form />
       </header>
       <div className="main-container">
-        {navigation}
+        {nav}
         {main}
       </div>
-
       <Modal />
       <Footer />
+      {navigation.state === "loading" && <Spinner />}
     </>
   );
 };
