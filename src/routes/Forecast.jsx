64f46b01c3d9dca7dd62c1646b1667831204fbba
Pawel <p.buczynski@outlook.com>
2023-05-14 17:38:31 +0200

    REFACTOR: change the structure of the displayed data, add a common header for all types of forecasts, display detailed forecasts as child components under the header, add a map with the location of the city, transfer data download to an external function

--- src/routes/Forecast.jsx
+++ src/routes/Forecast.jsx
@@ -1,49 +1,69 @@
-import { useContext, useLayoutEffect } from "react";
-import { useMatch, useLoaderData } from "react-router-dom";
-import { apiKey } from "../config";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Outlet, useLoaderData, useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
-import Forecast48Hours from "../components/Forecast48Hours";
-import Forecast8Days from "../components/Forecast8Days";
+import MapLocation from "../components/MapLocation";
+
+import fetchWeatherData from "../fetchWeatherData";
 
 export async function loader({ params }) {
-  const apiForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${params.lat}&lon=${params.lon}&exclude=current,minutely&appid=${apiKey}&units=metric&lang=pl`;
-
-  const weatherForecast = await fetch(apiForecast)
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      }
-      throw Error(`Błąd połączenia z serwerem. Kod: ${response.status}`);
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log("apiForecast ", data);
-      return data;
-    })
-    .catch((err) => {
-      return err;
-    });
-
-  return weatherForecast;
+  return await fetchWeatherData(params);
 }
 
 const Forecast = () => {
   const data = useLoaderData();
-  const { setWeatherForecast } = useContext(AppContext);
+  const params = useParams();
+  const { setAppBackground, setModal, setWeatherData } = useContext(AppContext);
+  const [hiddenMap, setHiddenMap] = useState(true);
+  const mapRef = useRef();
 
-  useLayoutEffect(() => {
-    console.log("useLayoutEffect Forecast");
-    setWeatherForecast(data);
-  });
+  const mapData = { lat: data.forecast.lat, lon: data.forecast.lon };
 
-  const location48Hours = useMatch("/48-hours/:lat/:lon");
-  const location8Days = useMatch("/8-days/:lat/:lon");
+  const handleMapBtn = () => setHiddenMap((hiddenMap) => !hiddenMap);
 
-  return location48Hours ? (
-    <Forecast48Hours />
-  ) : location8Days ? (
-    <Forecast8Days />
-  ) : null;
+  useEffect(() => {
+    setModal({ visible: false, header: {}, body: {} });
+    setWeatherData(data);
+    setAppBackground(data.current.weather[0].icon);
+    setHiddenMap(true);
+  }, [data, setAppBackground, setModal, setWeatherData]);
+
+  return (
+    <>
+      <header className="forecast__header">
+        <h1 className="forecast__header__title">
+          {decodeURIComponent(params.city)}
+          <span className="forecast__header__title__country">
+            , {data.current.sys.country}
+          </span>
+        </h1>
+        <button
+          className="btn-clear"
+          onClick={handleMapBtn}
+          type="button"
+          aria-expanded={hiddenMap ? false : true}
+          aria-controls="collapse-map"
+          aria-label="Show location of town on map">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="material-symbols-outlined"
+            viewBox="0 96 960 960"
+            width="24">
+            <path d="m600 914.46-240-84-172.153 66.615q-17.692 6.846-32.769-3.923-15.077-10.769-15.077-29.461V339.078q0-11.846 6.347-21.269 6.346-9.423 17.807-13.654L360 237.54l240 84 172.153-66.615q17.692-6.846 32.769 3.346 15.077 10.192 15.077 28.5v527.69q0 12.23-6.923 21.461-6.924 9.231-18.77 13.462L600 914.46Zm-29.999-73.383v-468l-180.002-62.923v468l180.002 62.923Zm59.998 0L760 798V324l-130.001 49.077v468ZM200 828l130.001-49.846v-468L200 354v474Zm429.999-454.923v468-468Zm-299.998-62.923v468-468Z" />
+          </svg>
+        </button>
+      </header>
+      <section
+        id="collapse-map"
+        className="forecast__map"
+        ref={mapRef}
+        style={hiddenMap ? null : { height: 240 }}>
+        {!hiddenMap && (
+          <MapLocation data={mapData} popup={false} dragging={false} />
+        )}
+      </section>
+      <Outlet />
+    </>
+  );
 };
 
 export default Forecast;
