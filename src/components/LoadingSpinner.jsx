4be7024ee4387cf4ea7f1b2c42ef3d39eb3fd504
Pawel <p.buczynski@outlook.com>
2023-05-14 17:14:28 +0200

    FEAT: add spinner for fetching data from server

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
