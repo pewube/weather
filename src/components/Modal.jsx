ba6a65b0c45de6b4acfcf49a60c3f288f379304e
Pawel <p.buczynski@outlook.com>
2023-05-14 17:15:10 +0200

    REAFCTOR: add svg instead of google material symbols

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,7 +16,13 @@ const Modal = () => {
         <article className="modal__content">
           <header className="modal__header">{modal.header}</header>
           <button onClick={removeModal} className="modal__btn-close btn-clear">
-            <span className="material-symbols-outlined">close</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="material-symbols-outlined"
+              viewBox="0 96 960 960"
+              width="24">
+              <path d="m256 856-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
           </button>
           <section className="modal__body">{modal.body}</section>
         </article>
