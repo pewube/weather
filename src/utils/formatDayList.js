6d5408624d78f6289cde0512fe9b6dd59f5455e0
Pawel <p.buczynski@outlook.com>
2023-05-15 20:31:33 +0200

    REFACTOR: rename variable

--- src/utils/formatDayList.js
+++ src/utils/formatDayList.js
@@ -3,10 +3,10 @@ import formatDayName from "./formatDayName";
 const formatDayList = (forecastArray) => {
   const daysArray = forecastArray
     ?.map((item) => formatDayName(item.dt))
-    .filter((item, index, array) => {
-      if (index === 0) {
+    .filter((item, idx, array) => {
+      if (idx === 0) {
         return item;
-      } else if (index > 0 && array[index] !== array[index - 1]) {
+      } else if (idx > 0 && array[idx] !== array[idx - 1]) {
         return item;
       }
       return null;
