bc8591ff8597cb3328e3bbe5768eb07fb7469f5c
Pawel <p.buczynski@outlook.com>
2023-05-22 17:11:41 +0200

    FEAT: add service worker config for vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,58 @@ export default defineConfig({
     }),
     react(),
     svgr({ svgrOptions: { icon: true } }),
-    VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "assets/images/leaflet/*.png",
+        "assets/images/bg/*-m-*.webp",
+      ],
+      injectRegister: "auto",
+      manifest: {
+        short_name: "Jaka pogoda ?",
+        name: "Jaka pogoda ?",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64",
+            type: "image/x-icon",
+          },
+          {
+            src: "logo16.png",
+            type: "image/png",
+            sizes: "16x16",
+            purpose: "any maskable",
+          },
+          {
+            src: "logo24.png",
+            type: "image/png",
+            sizes: "24x24",
+            purpose: "any maskable",
+          },
+          {
+            src: "logo32.png",
+            type: "image/png",
+            sizes: "32x32",
+            purpose: "any maskable",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+        ],
+        display: "standalone",
+        theme_color: "#0055d4",
+        background_color: "#ffffff",
+        lang: "pl",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,ttf,woff,woff2}"],
+      },
+    }),
   ],
 });
