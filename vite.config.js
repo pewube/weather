22fa066c3f9afc00f83ea46742b992a0d3a99450
Pawel <p.buczynski@outlook.com>
2023-05-23 12:00:24 +0200

    CHORE: enable development server local network path option

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [
     legacy({
       targets: ["defaults", "not IE 11"],
