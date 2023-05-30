eb5a1a0d39598d1d5a073e0d060f70284fa353ea
Pawel <p.buczynski@outlook.com>
2023-05-30 18:02:20 +0200

    REAFCTOR: add info if favorites list is empty

--- src/utils/formatFavoriteListModal.jsx
+++ src/utils/formatFavoriteListModal.jsx
@@ -1,11 +1,14 @@
 import CitiesList from "../components/CitiesList";
 
 const formatFavoriteListModal = (favoriteLocations, handleRemove) => {
-  const body = (
-    <ul className="header__favorites">
-      <CitiesList data={favoriteLocations} handleRemove={handleRemove} />
-    </ul>
-  );
+  const body =
+    favoriteLocations.length > 0 ? (
+      <ul className="header__favorites">
+        <CitiesList data={favoriteLocations} handleRemove={handleRemove} />
+      </ul>
+    ) : (
+      <p>Lista jest pusta</p>
+    );
   const header = <h2 className="modal__header__title">Ulubione lokalizacje</h2>;
 
   return { body, header };
